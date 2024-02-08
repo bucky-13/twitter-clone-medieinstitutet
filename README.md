abb1c50938d61e03529c60404ad1136a9587b071
bucky-13 <simon.stenstrom.013@gmail.com>
2024-02-08 10:54:16 +0100

    addec cors + mongodb

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # twitter-clone-medieinstitutet
+
 twitter clone for hackaton
+
+<!-- ## User
+name: string
+password: string
+email: string -->
+
+## messages
+
+message: string
+user: string
+\_id: id
--- app.js
+++ app.js
@@ -2,12 +2,32 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+let MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+MongoClient.connect('mongodb://127.0.0.1:27017').then((client) => {
+  console.log('Connected to Database');
+
+  let db = client.db('twitter-clone');
+
+  app.locals.db = db;
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+let messagesinterface = [
+  {
+    message: 'string',
+    user: 'string',
+    id: 'string',
+  },
+];
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
